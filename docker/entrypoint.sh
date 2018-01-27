3f53a09596e036d474304049bb0f7429667735c2
Bruce <bruce@haimi.com>
2018-01-27 22:19:19 +0800

    new entrypoint
    (cherry picked from commit e91344b)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
+#
+# Usage: $SELF start|stop|restart
+#
 
-./bin/kong-dashboard.js $@
+set -e
+
+case "$1" in
+  'start')
+        # run foreground
+        /app/bin/kong-dashboard.js $@
+        ;;
+
+  'stop')
+       kilall sshd
+        ;;
+
+  'restart')
+ 	    $0 stop
+        $0 start
+        ;;
+
+  *)
+        #echo "Usage: $SELF start|stop|restart"
+        #can not exit
+        #exit 1
+        ;;
+esac
+
+exec "$@"
